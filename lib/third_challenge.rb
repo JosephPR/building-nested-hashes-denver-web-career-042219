e0fcf91ed0d735336c344c15399335cbbf3770b7
Joseph Reilly <reilly4181@gmail.com>
2019-04-10 23:13:17 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -11,7 +11,9 @@ def third_challenge
       },
       :hero => {
 :name => "Romeo",
-      :age => "15"        },
+      :age => "15",
+      :status => "alive
+      "},
       :hero_friends => [
           {
          :name => "Benvolio",
